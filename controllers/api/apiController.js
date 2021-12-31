17d515254176a36cb486de4d57f24638ad6a7697
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-12-31 22:28:00 +0700

    Update api get soal ujian

--- controllers/api/apiController.js
+++ controllers/api/apiController.js
@@ -140,6 +140,166 @@ module.exports = {
                 .populate("dosen", "_id nama nip email jenisKelamin", "Dosen")
                 .populate("mataKuliah", "_id nama", "MataKuliah");
 
+              const abjads = ["A", "B", "C", "D", "E"];
+              let algorithm = "aes-256-cbc";
+
+              soalUjians.forEach((soalUjian) => {
+                if (soalUjian.soal.message !== "") {
+                  let ivSoal = base64decode(soalUjian.soal.iv);
+                  let keySoal = base64decode(soalUjian.soal.key);
+                  let messageSoal = base64decode(soalUjian.soal.message);
+
+                  let decipherSoal = crypto.createDecipheriv(
+                    algorithm,
+                    keySoal,
+                    ivSoal
+                  );
+                  let dataDecryptedSoal = decipherSoal.update(
+                    messageSoal,
+                    "hex",
+                    "utf-8"
+                  );
+                  let decryptedSoal =
+                    dataDecryptedSoal + decipherSoal.final("utf-8");
+
+                  soalUjian.soal.message = decryptedSoal.replace(`\r\n`, "");
+                }
+
+                if (soalUjian.soalGambar.message !== "") {
+                  let ivSoalGambar = base64decode(soalUjian.soalGambar.iv);
+                  let keySoalGambar = base64decode(soalUjian.soalGambar.key);
+                  let messageSoalGambar = base64decode(
+                    soalUjian.soalGambar.message
+                  );
+
+                  let decipherSoalGambar = crypto.createDecipheriv(
+                    algorithm,
+                    keySoalGambar,
+                    ivSoalGambar
+                  );
+                  let dataDecryptedSoalGambar = decipherSoalGambar.update(
+                    messageSoalGambar,
+                    "hex",
+                    "utf-8"
+                  );
+                  let decryptedSoalGambar =
+                    dataDecryptedSoalGambar + decipherSoalGambar.final("utf-8");
+
+                  soalUjian.soalGambar.message = decryptedSoalGambar.replace(
+                    `\r\n`,
+                    ""
+                  );
+                }
+
+                abjads.forEach((abjad) => {
+                  if (soalUjian["pilihan" + abjad].message !== "") {
+                    let ivPilihan = base64decode(
+                      soalUjian["pilihan" + abjad].iv
+                    );
+                    let keyPilihan = base64decode(
+                      soalUjian["pilihan" + abjad].key
+                    );
+                    let messagePilihan = base64decode(
+                      soalUjian["pilihan" + abjad].message
+                    );
+
+                    let decipherPilihan = crypto.createDecipheriv(
+                      algorithm,
+                      keyPilihan,
+                      ivPilihan
+                    );
+                    let dataDecryptedPilihan = decipherPilihan.update(
+                      messagePilihan,
+                      "hex",
+                      "utf-8"
+                    );
+                    let decryptedPilihan =
+                      dataDecryptedPilihan + decipherPilihan.final("utf-8");
+
+                    soalUjian["pilihan" + abjad].message =
+                      decryptedPilihan.replace(`\r\n`, "");
+                  }
+
+                  if (soalUjian["pilihanGambar" + abjad].message !== "") {
+                    let ivPilihanGambar = base64decode(
+                      soalUjian["pilihanGambar" + abjad].iv
+                    );
+                    let keyPilihanGambar = base64decode(
+                      soalUjian["pilihanGambar" + abjad].key
+                    );
+                    let messagePilihanGambar = base64decode(
+                      soalUjian["pilihanGambar" + abjad].message
+                    );
+
+                    let decipherPilihanGambar = crypto.createDecipheriv(
+                      algorithm,
+                      keyPilihanGambar,
+                      ivPilihanGambar
+                    );
+                    let dataDecryptedPilihanGambar =
+                      decipherPilihanGambar.update(
+                        messagePilihanGambar,
+                        "hex",
+                        "utf-8"
+                      );
+                    let decryptedPilihanGambar =
+                      dataDecryptedPilihanGambar +
+                      decipherPilihanGambar.final("utf-8");
+
+                    soalUjian["pilihanGambar" + abjad].message =
+                      decryptedPilihanGambar.replace(`\r\n`, "");
+                  }
+                });
+
+                if (soalUjian.kunciJawaban.message !== "") {
+                  let ivKunciJawaban = base64decode(soalUjian.kunciJawaban.iv);
+                  let keyKunciJawaban = base64decode(
+                    soalUjian.kunciJawaban.key
+                  );
+                  let messageKunciJawaban = base64decode(
+                    soalUjian.kunciJawaban.message
+                  );
+
+                  let decipherKunciJawaban = crypto.createDecipheriv(
+                    algorithm,
+                    keyKunciJawaban,
+                    ivKunciJawaban
+                  );
+                  let dataDecryptedKunciJawaban = decipherKunciJawaban.update(
+                    messageKunciJawaban,
+                    "hex",
+                    "utf-8"
+                  );
+                  let decryptedKunciJawaban =
+                    dataDecryptedKunciJawaban +
+                    decipherKunciJawaban.final("utf-8");
+
+                  soalUjian.kunciJawaban.message = decryptedKunciJawaban;
+                }
+
+                delete soalUjian.soalGambar._doc._id;
+                delete soalUjian.soalGambar._doc.iv;
+                delete soalUjian.soalGambar._doc.key;
+
+                delete soalUjian.soal._doc._id;
+                delete soalUjian.soal._doc.iv;
+                delete soalUjian.soal._doc.key;
+
+                abjads.forEach((abjad) => {
+                  delete soalUjian["pilihan" + abjad]._doc._id;
+                  delete soalUjian["pilihan" + abjad]._doc.iv;
+                  delete soalUjian["pilihan" + abjad]._doc.key;
+
+                  delete soalUjian["pilihanGambar" + abjad]._doc._id;
+                  delete soalUjian["pilihanGambar" + abjad]._doc.iv;
+                  delete soalUjian["pilihanGambar" + abjad]._doc.key;
+                });
+
+                delete soalUjian.kunciJawaban._doc._id;
+                delete soalUjian.kunciJawaban._doc.iv;
+                delete soalUjian.kunciJawaban._doc.key;
+              });
+
               res.status(200).json({
                 status: "success",
                 data: soalUjians,
